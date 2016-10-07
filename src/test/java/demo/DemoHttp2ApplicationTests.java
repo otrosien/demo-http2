0fc3f86f4a6710b68fe46f816313f20bcfd4e8f8
Oliver Trosien <otrosien@epages.com>
2016-10-07 16:57:43 +0200

    okhttp client test

--- src/test/java/demo/DemoHttp2ApplicationTests.java
+++ src/test/java/demo/DemoHttp2ApplicationTests.java
@@ -1,18 +1,32 @@
 package demo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = DemoHttp2Application.class)
-@WebAppConfiguration
+import okhttp3.OkHttpClient;
+import okhttp3.Protocol;
+import okhttp3.Request;
+import okhttp3.Response;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DemoHttp2Application.class, webEnvironment = WebEnvironment.RANDOM_PORT)
 public class DemoHttp2ApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @LocalServerPort
+    private int port;
+
+    @Test
+    public void testHttp2Connect() throws Exception {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url("https://127.0.0.1:" + port + "/").build();
+        Response response = client.newCall(request).execute();
+        assertThat(response.protocol()).isEqualTo(Protocol.HTTP_2);
+    }
 
 }
